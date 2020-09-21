81e7ec4a29093b096085642fd57b573620773816
Felipe Vieira <felipevieirab@gmail.com>
2020-09-21 09:36:34 -0300

    first_commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingSearchComponent } from './components/landing-search/landing-search.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+
+const routes: Routes = [
+  { path: '', component: TopBarComponent },
+  { path: 'users/:username', component: LandingSearchComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingSearchComponent } from './components/landing-search/landing-search.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TopSearchBarComponent } from './top-search-bar/top-search-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingSearchComponent,
+    TopBarComponent,
+    TopSearchBarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
